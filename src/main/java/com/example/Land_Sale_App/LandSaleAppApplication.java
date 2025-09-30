552d4fd53c6acd5beaf55e97e038dc1dc10f1491
Nadeesha-D-Shalom <nadeeshashalom1@gmail.com>
2025-10-01 01:16:00 +0530

    v 1.01.01

---
+++ src/main/java/com/example/Land_Sale_App/LandSaleAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.Land_Sale_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandSaleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LandSaleAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Land_Sale_App/LandSaleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Land_Sale_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandSaleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
